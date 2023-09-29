016f9436617db4a274a03e8f4324a79d917cdceb
Rutvik-Kalathiya <rutvik.kalathiya@purpleleafstrategy.com>
2023-09-29 14:55:51 +0200

    Cancel button added

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -53,94 +53,85 @@ export default class Form extends Component {
       const{userData} = this.state
       userData.splice(i,1)
       this.setState({
-        userData:[...userData]
+        userData:[...userData],
+        isEditing: false 
       })
-      
     }
+
+    handleCancel = () => {
+      this.resetData();
+      this.setState({
+          isEditing: false,
+          editDetails: {},
+      });
+  }
 
   render() {
     const {userData, userDetails, isEditing} = this.state
     return (
       <div>
         <h3 style={{textAlign:'center'}}>Contact Form</h3>
+        <div className="container">
+            <label>First Name</label>
+            <input type="text" id="fname" name="fname" value={userDetails.fname} placeholder="Your name.." onChange={(e) =>this.handleChange(e)} />
+            <label>Last Name</label>
+            <input type="text" id="lname" name="lname" placeholder="Your last name.." onChange={(e) =>this.handleChange(e)} value={userDetails.lname}/>
 
-<div className="container">
-    <div>
-    <label>First Name</label>
-    <input type="text" id="fname" name="fname" value={userDetails.fname} placeholder="Your name.." onChange={(e) =>this.handleChange(e)} />
-    </div>
-    <label>Last Name</label>
-    <input type="text" id="lname" name="lname" placeholder="Your last name.." onChange={(e) =>this.handleChange(e)} value={userDetails.lname}/>
-
-    <label>Country</label>
-    <select id="country" name="country" onChange={(e) =>this.handleChange(e)} value={userDetails.country}>
-      <option value="">Select your country</option>
-      <option value="india">India</option>
-      <option value="brazil">Brazil</option>
-      <option value="australia">Australia</option>
-      <option value="canada">Canada</option>
-      <option value="usa">USA</option>
-    </select>
-
-    <button type="button" value="Submit" onClick={() => this.handleSubmit()}>Submit </button>
-    
-
-</div>
-<h2 style={{textAlign:"center"}}>User Data Table</h2>
-
-<table>
-  <tbody>
-  <tr>
-    <th>First Name</th>
-    <th>Last Name</th>
-    <th>Country</th>
-    <th>Actions</th>
-  </tr>
-
-  {userData.map((value, i) => {
-              return (<tr key={i}>
-                <td>{isEditing && this.state.index === i ? (
-                  <input type="text" name="fname" value={userDetails.fname} onChange={(e) => this.handleChange(e)} />
-                ) : (
-                  value.fname
-                )}
-                </td>
-                <td>{isEditing && this.state.index === i ? (
-                  <input type="text" name="lname" value={userDetails.lname} onChange={(e) => this.handleChange(e)} />
-                ) : (
-                  value.lname
-                )}
-                </td>
-                <td>{isEditing && this.state.index === i ? (
-                  <select name="country" onChange={(e) => this.handleChange(e)} value={userDetails.country}>
-                    <option value="">Select your country</option>
-                    <option value="india">India</option>
-                    <option value="brazil">Brazil</option>
-                    <option value="australia">Australia</option>
-                    <option value="canada">Canada</option>
-                    <option value="usa">USA</option>
-                  </select>
-                ) : (
-                  value.country
-                )}
-                </td>
-                <td>
-                  {isEditing && this.state.index === i ? (
+            <label>Country</label>
+            <select id="country" name="country" onChange={(e) =>this.handleChange(e)} value={userDetails.country}>
+              <option value="">Select your country</option>
+              <option value="india">India</option>
+              <option value="brazil">Brazil</option>
+              <option value="australia">Australia</option>
+              <option value="canada">Canada</option>
+              <option value="usa">USA</option>
+            </select>
+            <button type="button" value="Submit" onClick={() => this.handleSubmit()}>Submit </button>
+        </div>
+        <h2 style={{textAlign:"center"}}>User Data Table</h2>
+        <table>
+          <tbody>
+          <tr>
+            <th>First Name</th>
+            <th>Last Name</th>
+            <th>Country</th>
+            <th>Actions</th>
+          </tr>
+          {userData.map((value, i) => {
+            return (<tr key={i}>
+              <td>{isEditing && this.state.index === i ? (
+                <input type="text" name="fname" value={userDetails.fname} onChange={(e) => this.handleChange(e)} />
+              ) : ( value.fname )}
+              </td>
+              <td>{isEditing && this.state.index === i ? (
+                <input type="text" name="lname" value={userDetails.lname} onChange={(e) => this.handleChange(e)} />
+              ) : ( value.lname )}
+              </td>
+              <td>{isEditing && this.state.index === i ? (
+                <select name="country" onChange={(e) => this.handleChange(e)} value={userDetails.country}>
+                  <option value="">Select your country</option>
+                  <option value="india">India</option>
+                  <option value="brazil">Brazil</option>
+                  <option value="australia">Australia</option>
+                  <option value="canada">Canada</option>
+                  <option value="usa">USA</option>
+                </select>
+              ) : ( value.country )}
+              </td>
+              <td>{isEditing && this.state.index === i ? (
+                  <>
                     <button type='button' style={{ margin: 5 }} onClick={() => this.handleSubmit()}>Save</button>
-                  ) : (
-                    <>
-                      <button type='button' style={{ margin: 5 }} onClick={() => this.handleEdit(i)}>Edit</button>
-                      <button type='button' style={{ margin: 5 }} onClick={() => this.handleDelete(i)}>Delete</button>
-                    </>
-                  )}
-                </td>
-              </tr>)
-            }
-            )}
-  </tbody>
-  
-</table>
-        
+                    <button type='button' style={{ margin: 5 }} onClick={() => this.handleCancel()}>Cancel</button>
+                  </>
+                ) : ( <>
+                        <button type='button' style={{ margin: 5 }} onClick={() => this.handleEdit(i)}>Edit</button>
+                        <button type='button' style={{ margin: 5 }} onClick={() => this.handleDelete(i)}>Delete</button>
+                      </>
+                )}
+              </td>
+            </tr>)})}
+          </tbody>
+        </table>
       </div>
     )
   }
